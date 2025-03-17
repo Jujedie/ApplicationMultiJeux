92b75342a7c38a57dd7be2cfe15f976c720651a3
Nahel <nahel.kochat@laposte.net>
2025-03-17 15:13:15 +0100

    début conception jeu de la vie

--- app/src/main/java/com/example/applicationmultijeux/JeuDeLaVieActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/JeuDeLaVieActivity.java
@@ -1,8 +1,19 @@
 package com.example.applicationmultijeux;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class JeuDeLaVieActivity extends Activity
 {
+    private boolean[][] tableauJeuVie;
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
 
 }
