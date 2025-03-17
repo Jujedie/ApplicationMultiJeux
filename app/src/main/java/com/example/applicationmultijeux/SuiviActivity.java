33bd5ee7020383416778fdf01f419d6e96ded8eb
Julien Oyer <oyer.ludovic@gmail.com>
2025-03-17 15:49:09 +0100

    modifs suivi

--- app/src/main/java/com/example/applicationmultijeux/SuiviActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/SuiviActivity.java
@@ -1,8 +1,17 @@
 package com.example.applicationmultijeux;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SuiviActivity extends Activity
 {
+    protected boolean isInAcceuil;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.isInAcceuil = true;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil_suivi);
+    }
 }
