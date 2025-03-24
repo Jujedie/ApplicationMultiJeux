36c04b4905a40bb2c30baa810eed2dbebf237fa7
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-03-24 16:06:29 +0100

    maj

--- app/src/main/java/com/example/applicationmultijeux/jeudelavie/AccueilJeuDeLaVieActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/jeudelavie/AccueilJeuDeLaVieActivity.java
@@ -4,6 +4,8 @@ import com.example.applicationmultijeux.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AccueilJeuDeLaVieActivity extends Activity
 {
@@ -13,5 +15,16 @@ public class AccueilJeuDeLaVieActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil_jeudelavie);
+
+        Spinner spinner = (Spinner) findViewById(R.id.accueilJDLV3);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+        (
+            this,
+            R.array.lstTaillesGrilles,
+            android.R.layout.simple_spinner_item
+        );
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
