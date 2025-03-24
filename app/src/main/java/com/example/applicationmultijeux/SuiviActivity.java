2b006ada6b219f99a34df4d708aa90a3494d9a1d
Julien Oyer <oyer.ludovic@gmail.com>
2025-03-24 10:03:09 +0100

    Ajout de fonctionnalités sur l'Activité suivi

--- app/src/main/java/com/example/applicationmultijeux/SuiviActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/SuiviActivity.java
@@ -2,16 +2,27 @@ package com.example.applicationmultijeux;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.content.Intent;
+
 
 public class SuiviActivity extends Activity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suivi);
+
+        // Récupérer les données de l'intent
+        Intent intent = getIntent();
+        String forme = intent.getStringExtra("forme");
+        String niveau= intent.getStringExtra("niveau");
 
 
+    }
 
+    public void genererFigure(String forme, String niveau)
+    {
+        
     }
 }
