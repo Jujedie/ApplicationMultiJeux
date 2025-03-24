4ee82338ab319ec84119c316c1a2f6d43c5d0cd7
Julien Oyer <oyer.ludovic@gmail.com>
2025-03-24 10:04:29 +0100

    Ajout de fonctionnalités sur l'Activité suivi

--- app/src/main/java/com/example/applicationmultijeux/SuiviActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/SuiviActivity.java
@@ -23,6 +23,29 @@ public class SuiviActivity extends Activity
 
     public void genererFigure(String forme, String niveau)
     {
-        
+
     }
+
+    /*
+    super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil_suivi);
+
+        Spinner spinner = (Spinner) findViewById(R.id.SpinnerForme);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.listeForme,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner = (Spinner) findViewById(R.id.SpinnerNiveau);
+        adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.listeNiveau,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);    
+     */
 }
