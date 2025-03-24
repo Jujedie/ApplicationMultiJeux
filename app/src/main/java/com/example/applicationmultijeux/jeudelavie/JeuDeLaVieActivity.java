d281ba54909b07625435d3553db2589aea659c3a
Nahel <nahel.kochat@laposte.net>
2025-03-24 16:49:00 +0100

    métier jeu de la vie

--- app/src/main/java/com/example/applicationmultijeux/jeudelavie/JeuDeLaVieActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/jeudelavie/JeuDeLaVieActivity.java
@@ -4,14 +4,49 @@ import com.example.applicationmultijeux.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class JeuDeLaVieActivity extends Activity
 {
+    private Grille      grilleJeuVie;
     private boolean[][] tableauJeuVie;
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jeudelavie);
+
+        View view = findViewById(R.id.view);
+
+
+
+        boolean[][] avtDernierTab = new boolean[2][2];
+        boolean[][] dernierTab    = new boolean[2][3];
+        int     nbTabPareil   = 0;
+
+        while (nbTabPareil < 1000)
+        {
+            grilleJeuVie.etatSuivant();
+            Cellule[] tabCellule = (Cellule[]) grilleJeuVie.getSetCellule().toArray();
+            for (int lig = 0; lig < tableauJeuVie.length; lig++)
+            {
+                for (int col = 0; col < tableauJeuVie.length; col++)
+                {
+                    tableauJeuVie[lig][col] = tabCellule[lig*9+col].getEtat();
+                }
+            }
+
+            if (tableauJeuVie.equals(dernierTab) || tableauJeuVie.equals(avtDernierTab))
+                nbTabPareil++;
+            else
+                nbTabPareil=0;
+
+            avtDernierTab = dernierTab.clone();
+            dernierTab  =   this.tableauJeuVie.clone();
+        }
+
+
     }
+
+
 }
