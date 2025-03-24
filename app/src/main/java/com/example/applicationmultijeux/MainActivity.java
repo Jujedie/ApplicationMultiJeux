6551b29767030024e58fc9801ba2a4154f594fc5
Julien Oyer <oyer.ludovic@gmail.com>
2025-03-24 09:24:35 +0100

    changement de la gestion de l'acceuil de l'activité suivi

--- app/src/main/java/com/example/applicationmultijeux/MainActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.applicationmultijeux;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity
 {
@@ -22,9 +24,28 @@ public class MainActivity extends Activity
 
     }
 
-    public void accueilSuivi()
+    public void accueilSuivi(Bundle savedInstanceState)
     {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil_suivi);
 
+        Spinner spinner = (Spinner) findViewById(R.id.SpinnerForme);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.listeForme,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner = (Spinner) findViewById(R.id.SpinnerNiveau);
+        adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.listeNiveau,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     public void quitterApplication()
--- app/src/main/java/com/example/applicationmultijeux/SuiviActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/SuiviActivity.java
@@ -7,32 +7,11 @@ import android.widget.Spinner;
 
 public class SuiviActivity extends Activity
 {
-    protected boolean isInAcceuil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        this.isInAcceuil = true;
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accueil_suivi);
 
-        Spinner spinner = (Spinner) findViewById(R.id.SpinnerForme);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.listeForme,
-                android.R.layout.simple_spinner_item
-        );
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        spinner = (Spinner) findViewById(R.id.SpinnerNiveau);
-        adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.listeNiveau,
-                android.R.layout.simple_spinner_item
-        );
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
 
     }
 }
