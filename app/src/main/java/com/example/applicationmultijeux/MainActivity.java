48a184375f8d20a2fba9e36b0c9be81195047a54
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-03-24 10:01:14 +0100

    maj

--- app/src/main/java/com/example/applicationmultijeux/MainActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.applicationmultijeux;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity
 {
-    @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -14,12 +16,30 @@ public class MainActivity extends Activity
 
     public void accueilTicTacToe()
     {
+        Button btnAccueilTicTacToe = findViewById(R.id.tv2);
 
+        btnAccueilTicTacToe.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, TicTacToeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void accueilJeuDeLaVie()
     {
+        Button btnAccueilJeuDeLaVie = findViewById(R.id.tv3);
 
+        btnAccueilJeuDeLaVie.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, JeuDeLaVieActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void accueilSuivi()
