d63354231d00fff0bcbd5a3afa75f5eaeeb33bb6
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-03-25 09:31:39 +0100

    aa

--- app/src/main/java/com/example/applicationmultijeux/jeudelavie/AccueilJeuDeLaVieActivity.java
+++ app/src/main/java/com/example/applicationmultijeux/jeudelavie/AccueilJeuDeLaVieActivity.java
@@ -8,18 +8,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class AccueilJeuDeLaVieActivity extends Activity
 {
-    private boolean[][] tableauJeuVie;
+    private Spinner spinnerTailleGrille;
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil_jeudelavie);
 
-        Spinner spinner = (Spinner) findViewById(R.id.accueilJDLV3);
+        spinnerTailleGrille = (Spinner) findViewById(R.id.accueilJDLV3);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
         (
             this,
@@ -28,20 +29,23 @@ public class AccueilJeuDeLaVieActivity extends Activity
         );
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        spinnerTailleGrille.setAdapter(adapter);
 
         lancerJeuDeLaVie();
     }
 
     public void lancerJeuDeLaVie()
     {
-        Button btnLancer = findViewById(R.id.accueilJDLV6);
+        Button   btnLancer  = findViewById(R.id.accueilJDLV6);
+        EditText edtDensite = (EditText) findViewById(R.id.accueilJDLV5);
 
         btnLancer.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
                 Intent intent = new Intent(AccueilJeuDeLaVieActivity.this, JeuDeLaVieActivity.class);
+                intent.putExtra("TailleGrille", (String) spinnerTailleGrille.getSelectedItem());
+                intent.putExtra("DensiteCellulesVivantes", edtDensite.getText());
                 startActivity(intent);
             }
         });
